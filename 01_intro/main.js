868a3968801d04d7d178b03ecad00b43be4d88ce
Olia <oliaopanasiukk@gmail.com>
2024-05-20 23:04:51 +0300

    initial commit

---
+++ 01_intro/main.js
@@ -0,0 +1,53 @@
+// task 1
+// let res = prompt("Enter the number:");
+// console.log(res**2);
+
+// task 2
+// let n1 = +prompt("Enter the first number:");
+// let n2 = +prompt("Enter the first number:");
+// console.log((n1 + n2) / 2);
+
+// task 3
+// let n = prompt("Enter the side of the square:");
+// console.log("S = " + n**2);
+
+// task 4
+// const mile = 0.621371;
+// let km = prompt('Enter kilometers to convert:');
+// alert(parseFloat(km * mile).toFixed(6) + ' kilometers');
+
+// task 5
+// let a = +prompt('Enter the first number:');
+// let b = +prompt('Enter the second number:');
+// console.log(a + ' + ' + b + ' = ' + (a + b));
+// console.log(a + ' - ' + b + ' = ' + (a - b));
+// console.log(a + ' * ' + b + ' = ' + (a * b));
+
+// task 6
+// let a = +prompt('Enter a:');
+// let b = +prompt('Enter b:');
+// let res = -b / a
+// alert("X: " + res);
+
+// task 7
+// let h = +prompt('Enter hours:');
+// let m = +prompt('Enter the minutes:');
+// hour = 23 - h;
+// min = 60 - m;
+// alert('Until the next day: ' + hour + ' : ' + min);
+
+// task 8
+// let n = +prompt('Enter a 3-digit number:');
+// i = parseInt(n/10);
+// i = i % 10;
+// alert(i);
+
+// task 9
+// let n = +prompt('Enter a 5-digit number:');
+// x = n % 10;
+// alert(x +''+ parseInt(n / 10));
+
+// task 10
+// let n = +prompt('The total amount of sales for the month:');
+// salary = 250 + (n/100)*10;
+// alert('Salary: '+ salary + '$');
