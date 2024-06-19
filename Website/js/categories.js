0d67aef9763d51d21bc315828d65d1e66bdd7739
Olia <oliaopanasiukk@gmail.com>
2024-06-19 11:41:23 +0300

    fetch categories

--- Website/js/categories.js
+++ Website/js/categories.js
@@ -1,15 +1,23 @@
-const url = "https://pd322.itstep.click/api/Category/CategoryGetAsync";
+const server = 'https://pd322.itstep.click/';
+let url = `${server}api/Category/CategoryGetAsync`;
+let tbody = document.querySelector("#categories tbody");
+let defaultImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzNHDxW4Ms3wQ_SizDhUn7O9ivXu7BM2pfTw&s";
+tbody.innerHTML = "";
+
 axios.get(url)
     .then(resp => {
-        let tbody = document.querySelector('#categories tbody');
-        tbody.innerHTML = '';
-
-        resp.data.forEach(({ id, name, description }) => {
-            let row = document.createElement('tr');
-            row.innerHTML = `<td>${id}</td>
-                             <td>${name}</td>
-                             <td>${description}</td>`;
-            tbody.appendChild(row);
+        let categories = resp.data;
+        categories.forEach(c => {
+            let img = c.imagePath ? `${server}images/320_${c.imagePath}` : defaultImage;
+            let row = `
+                    <tr>
+                        <th scope="row">${c.id}</th>
+                        <td>${c.name}</td>
+                        <td>${c.description}</td>
+                        <td><img src="${img}" width="75"/></td>
+                    </tr>
+                `;
+            tbody.innerHTML += row;
         });
     })
     .catch(error => {
